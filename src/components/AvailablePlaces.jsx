c070554585caa44df2be397c65b32a2f727c78f0
anna.bielskaya <anna.bielskaya@gmail.com>
2026-05-18 13:53:43 +0300

    Added error message in case of an unsuccessfunl request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,29 +1,56 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import axios from "axios";
+import ErrorMessage from "./ErrorMessage.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
   const [places, setPlaces] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      const response = await axios.get("http://localhost:3000/places/");
-      setPlaces(response.data.places);
-      setIsLoading(false);
+
+      try {
+        const response = await axios.get("http://localhost:300/places/");
+
+        if (response.ok) {
+          setErrorMessage("Failed to fetch places.");
+          console.error("Failed to fetch places:");
+        }
+
+        setPlaces(response.data.places);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch places:", error);
+        setErrorMessage("Failed to fetch places.", error);
+        setIsLoading(false);
+        return;
+      }
     };
 
     fetchData();
   }, []);
 
   return (
-    <Places
-      title="Available Places"
-      places={places}
-      isLoading={isLoading}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
+    <>
+      {errorMessage && (
+        <ErrorMessage
+          title="Error"
+          message={errorMessage}
+          onConfirm={() => setErrorMessage(null)}
+        />
+      )}
+      {!errorMessage && (
+        <Places
+          title="Available Places"
+          places={places}
+          isLoading={isLoading}
+          fallbackText="No places available."
+          onSelectPlace={onSelectPlace}
+        />
+      )}
+    </>
   );
 }
